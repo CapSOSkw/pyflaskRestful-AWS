99036415bf31e6db3111d1018e8c402b31a2600b
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2017-11-21 09:20:35 -0500

    Update count_fixed_user.py

--- count_fixed_user.py
+++ count_fixed_user.py
@@ -29,23 +29,33 @@ import operator
 warnings.filterwarnings('ignore')
 
 
-#connect to mysql, database is mytest, table is merge_8910
+#connect to mysql
 db = pymysql.connect("localhost", "root", "0702","mytest")
 cnx = create_engine('mysql+pymysql://root:0702@localhost/mytest',echo=False)
 cursor = db.cursor()
 
 
 def get_fleet(id):
-    SQL = "select fleet from oneyear where fleet_id="+str(id)
+    SQL = "select fleet from oneyear where fleet_id=" + str(id)
     cursor.execute(SQL)
     temp = cursor.fetchone()[0]
     return temp
 
 def get_username(id):
-    SQL = "select cust_fullname from oneyear WHERE customer_id="+str(id)
+    SQL = "select cust_fullname from oneyear WHERE customer_id=" + str(id)
     cursor.execute(SQL)
     temp = cursor.fetchone()[0]
     return temp
+
+def get_customer_id(cust_fullname):
+    try:
+        SQL = "select DISTINCT customer_id from oneyear WHERE cust_fullname=" + "'" + cust_fullname + "'"
+        cursor.execute(SQL)
+        customer_id = cursor.fetchone()[0]
+    except:
+        customer_id = -1
+    print(customer_id)
+    return customer_id
 
 driver_data = []
 user_data = []
@@ -58,44 +68,27 @@ for i in pd.read_sql("select customer_id from oneyear", cnx).values:
 
 set_user_data = set(user_data)
 
-#
-# cache = []
-#
-# for i in list(set_user_data)[1:2]:
-#
-#     SQL = "select fleet_id from oneyear WHERE customer_id="+str(i)
-#     cursor.execute(SQL)
-#     temp = cursor.fetchall()
-#     for i in temp:
-#         cache.append(i[0])
-# xx = set(cache)
-#
-# for i in list(xx):
-#     print(get_fleet(i),cache.count(int(i)))
 dict = OrderedDict()
 
 for user_id in list(set_user_data):
-    print(float(user_id)/float(len(list(set_user_data)))*100)
     temp_dict = OrderedDict()
     cache = []
-    SQL = "select fleet_id from oneyear WHERE customer_id="+str(user_id)
+    SQL = "select fleet_id from oneyear WHERE customer_id=" + str(user_id)
     cursor.execute(SQL)
     temp_fleet_id = cursor.fetchall()
     for item in temp_fleet_id:
         cache.append(item[0])
+        
     set_cache = set(cache)
 
     for i in list(set_cache):
         temp_dict[str(get_fleet(i))] = cache.count(int(i))
-    temp_dict = sorted(temp_dict.items(), key=operator.itemgetter(1), reverse=True)
-    # temp_dict = sorted(temp_dict.items(), key=lambda x:x[1],reverse=True)
-        # temp_dict.append({str(get_fleet(i)):cache.count(int(i))})
+        
+    temp_dict = sorted(temp_dict.items(), key=operator.itemgetter(1), reverse=True) # Sort dictionary by values
     dict[str(get_username(user_id))] = temp_dict
-
-# with open('fixed_users2.json', 'w') as outfile:
-#     json.dump(dict, outfile, indent=4, sort_keys=True)
+    
 
 df = pd.DataFrame.from_dict(dict, orient='index')
-df.to_csv('11_16.csv')
-# df2 = df.iloc[:,0:5]
-# df2.to_csv("fixed_users.csv")
+df['USER'] = df['USER'].apply(lambda x: re.sub("\s\s+"," ",x).strip())
+df['USER_ID'] = df['USER'].apply(lambda x:get_customer_id(x))
+df.to_csv('*****.csv')
